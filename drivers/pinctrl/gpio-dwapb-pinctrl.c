6052808954779e1f72dd32c5b0415c80887dbd70
Omar Al-Wadi <oalwadi@elvees.com>
2021-08-25 12:22:15 +0300

    gpio: dwapb: Move gpio/gpio-dwapb.c to pinctrl/ directory
    Since most of the other implementations of pinctrl functionality are in
    drivers/pinctrl/, so we should move gpio-dwapb into it and rename the
    driver to gpio-dwapb-pinctrl.
    
    This patch also moves bindings gpio/snps-dwapb-gpio.txt to
    pinctrl/snps-dwapb-gpio.txt
    
    Issue: #MCOM03BUP-191
    Change-Id: I16baf7a405a6cae5ef9bb9197329d7a9292cf3d6

--- drivers/pinctrl/gpio-dwapb-pinctrl.c
+++ drivers/pinctrl/gpio-dwapb-pinctrl.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2011 Jamie Iles
+ * Copyright 2021 RnD Center "ELVEES", JSC
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
