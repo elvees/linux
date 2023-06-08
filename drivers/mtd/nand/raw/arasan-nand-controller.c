31c78472989ab4afce84f6f616990ecda6da1279
Omar Al-Wadi <oalwadi@elvees.com>
2023-06-08 18:19:55 +0300

    mtd: rawnand: arasan: Add MCom-03 supported version
    Issue: #MCOM03SW-2009
    Signed-off-by: Omar Al-Wadi <oalwadi@elvees.com>
    Change-Id: I161c1523ea8f5259ece43f88f18e33f37fdfbd83

--- drivers/mtd/nand/raw/arasan-nand-controller.c
+++ drivers/mtd/nand/raw/arasan-nand-controller.c
@@ -1357,6 +1357,9 @@ static const struct of_device_id anfc_ids[] = {
 	{
 		.compatible = "arasan,nfc-v3p10",
 	},
+	{
+		.compatible = "arasan,nfc-v3p16"
+	},
 	{}
 };
 MODULE_DEVICE_TABLE(of, anfc_ids);
