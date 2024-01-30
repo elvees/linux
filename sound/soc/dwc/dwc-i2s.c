04c59cffed4b86a72718b0f45620e0cc1743700a
Oleg Ivanov <oaivanov@elvees.com>
2024-01-30 12:07:43 +0300

    ASoC: dwc: Don't print messages in interrupt handler
    The interrupt handler prints messages about RX/TX FIFO overrun
    events. This increases interrupt processing time and causes even
    more RX/TX FIFO overruns. As a result CPU doesn't have enough time
    to process all audio data, some data is lost.
    
    This commit removes printing in the interrupt handler, but adds the
    ability to read the number of TX/RX overruns using DEBUG FS.
    
    Issue: #MCOM03SW-2505
    Issue: #MCOM03SW-3501
    Change-Id: Id5edec37e4fd05d3b79a59dfa0d255fb001c4339
    Signed-off-by: Oleg Ivanov <oaivanov@elvees.com>
    (cherry picked from commit 94281218e982ae84c4b0747b136f8bc5394c51b7)

--- sound/soc/dwc/dwc-i2s.c
+++ sound/soc/dwc/dwc-i2s.c
@@ -28,6 +28,35 @@
 #include <sound/dmaengine_pcm.h>
 #include "local.h"
 
+#ifdef CONFIG_DEBUG_FS
+static void i2s_remove_debugfs(struct dw_i2s_dev *dev)
+{
+	debugfs_remove_recursive(dev->debugfs.root);
+}
+
+static void i2s_create_debugfs(struct dw_i2s_dev *dev)
+{
+	dev->debugfs.root = debugfs_create_dir("dwc-i2s", NULL);
+
+	debugfs_create_u32("tx_fifo_overrun", 0444, dev->debugfs.root,
+			   &dev->debugfs.tx_fifo_overrun);
+	debugfs_create_u32("tx_fifo_empty", 0444, dev->debugfs.root,
+			   &dev->debugfs.tx_fifo_empty);
+	debugfs_create_u32("rx_fifo_overrun", 0444, dev->debugfs.root,
+			   &dev->debugfs.rx_fifo_overrun);
+	debugfs_create_u32("rx_data_available", 0444, dev->debugfs.root,
+			   &dev->debugfs.rx_data_available);
+}
+#else
+static void i2s_remove_debugfs(struct dw_i2s_dev *dev)
+{
+}
+
+static void i2s_create_debugfs(struct dw_i2s_dev *dev)
+{
+}
+#endif
+
 static inline void i2s_write_reg(void __iomem *io_base, int reg, u32 val)
 {
 	writel(val, io_base + reg);
@@ -120,6 +149,7 @@ static irqreturn_t i2s_irq_handler(int irq, void *dev_id)
 		 */
 		if ((isr[i] & ISR_TXFE) && (i == 0) && dev->use_pio) {
 			dw_pcm_push_tx(dev);
+			dev->debugfs.tx_fifo_empty++;
 			irq_valid = true;
 		}
 
@@ -129,18 +159,19 @@ static irqreturn_t i2s_irq_handler(int irq, void *dev_id)
 		 */
 		if ((isr[i] & ISR_RXDA) && (i == 0) && dev->use_pio) {
 			dw_pcm_pop_rx(dev);
+			dev->debugfs.rx_data_available++;
 			irq_valid = true;
 		}
 
 		/* Error Handling: TX */
 		if (isr[i] & ISR_TXFO) {
-			dev_err_ratelimited(dev->dev, "TX overrun (ch_id=%d)\n", i);
+			dev->debugfs.tx_fifo_overrun++;
 			irq_valid = true;
 		}
 
-		/* Error Handling: TX */
+		/* Error Handling: RX */
 		if (isr[i] & ISR_RXFO) {
-			dev_err_ratelimited(dev->dev, "RX overrun (ch_id=%d)\n", i);
+			dev->debugfs.rx_fifo_overrun++;
 			irq_valid = true;
 		}
 	}
@@ -968,6 +999,8 @@ static int dw_i2s_probe(struct platform_device *pdev)
 		}
 	}
 
+	i2s_create_debugfs(dev);
+
 	dev->i2s_reg_comp1 = I2S_COMP_PARAM_1;
 	dev->i2s_reg_comp2 = I2S_COMP_PARAM_2;
 	if (pdata) {
@@ -1051,6 +1084,8 @@ static void dw_i2s_remove(struct platform_device *pdev)
 
 	if (dev->capability & DW_I2S_MASTER)
 		clk_disable_unprepare(dev->clk);
+
+	i2s_remove_debugfs(dev);
 
 	reset_control_assert(dev->reset);
 	pm_runtime_disable(&pdev->dev);
