7e23e1526fee6f412dc1eb77fb8b1ce385e9c031
Vasiliy Zasukhin <vzasukhin@elvees.com>
2024-02-15 16:53:06 +0300

    dt-bindings: soc: Remove useless sybsystem enums
    RISC0, CORE and TOP subsystems are controlled by RISC0 firmware.
    Linux should not interact with them directly.
    
    Change-Id: Id7d1341784f8e5ab903d87b15eb0a448e7507b57
    Signed-off-by: Vasiliy Zasukhin <vzasukhin@elvees.com>

--- include/dt-bindings/soc/elvees,mcom03.h
+++ include/dt-bindings/soc/elvees,mcom03.h
@@ -1,26 +1,21 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
- * Copyright 2021 RnD Center "ELVEES", JSC
+ * Copyright 2021-2024 RnD Center "ELVEES", JSC
  */
 
 #ifndef __DT_BINDINGS_ELVEES_MCOM03_H
 #define __DT_BINDINGS_ELVEES_MCOM03_H
 
-/* The order of the subsystems corresponds to the order in the universal
- * register block (URB) of the service subsystem.
- */
+/* Subsystems that can be accessed from Linux */
 #define MCOM03_SUBSYSTEM_CPU		0
 #define MCOM03_SUBSYSTEM_SDR		1
 #define MCOM03_SUBSYSTEM_MEDIA		2
-#define MCOM03_SUBSYSTEM_CORE		3
-#define MCOM03_SUBSYSTEM_HSPERIPH	4
-#define MCOM03_SUBSYSTEM_LSPERIPH0	5
-#define MCOM03_SUBSYSTEM_LSPERIPH1	6
-#define MCOM03_SUBSYSTEM_DDR		7
-#define MCOM03_SUBSYSTEM_TOP		8
-#define MCOM03_SUBSYSTEM_RISC0		9
-#define MCOM03_SUBSYSTEM_SERVICE	10
-#define MCOM03_SUBSYSTEM_MAX		11
+#define MCOM03_SUBSYSTEM_HSPERIPH	3
+#define MCOM03_SUBSYSTEM_LSPERIPH0	4
+#define MCOM03_SUBSYSTEM_LSPERIPH1	5
+#define MCOM03_SUBSYSTEM_DDR		6
+#define MCOM03_SUBSYSTEM_SERVICE	7
+#define MCOM03_SUBSYSTEM_MAX		8
 
 #define SDR_RST_PCI0_REF		0
 #define SDR_RST_JESD0_TX_REF		1
