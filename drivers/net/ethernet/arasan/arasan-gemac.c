ce50e473c3d8aa6fbafe43ac412af7430f635d9c
Ilya Orazov <iorazov@elvees.com>
2024-04-03 11:58:05 +0300

    net: arasan-gemac: Rename compatible field to 'arasan,gemac'
    The driver is designed for generic Arasan GEMAC controller, so there
    shouldn't be 'elvees' in compatible field.
    
    Issue: #MCOM03SW-3235
    Issue: #MCOM03SW-2771
    Change-Id: Ic9779594d20694330c34ecedec06fb9f31ab6878
    Signed-off-by: Ilya Orazov <iorazov@elvees.com>
    (cherry picked from commit a219a3f2fa7aee1d3d1c2bd3490d38e7340537c3)

--- drivers/net/ethernet/arasan/arasan-gemac.c
+++ drivers/net/ethernet/arasan/arasan-gemac.c
@@ -1312,7 +1312,7 @@ static const struct net_device_ops arasan_gemac_netdev_ops = {
 
 #if defined(CONFIG_OF)
 static const struct of_device_id arasan_gemac_dt_ids[] = {
-	{ .compatible = "elvees,arasan-gemac" },
+	{ .compatible = "arasan,gemac" },
 	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, arasan_gemac_dt_ids);
