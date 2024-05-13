ae434991c65e57ac27d799042be9a0ff20398c65
Vladimir Terekhov <vterekhov@elvees.com>
2024-05-13 18:53:31 +0300

    drm: mali-dp: Add MCom-03 support
    This commit includes the following commits:
    commit 8111bd078f0b ("drm: mali-dp: Print underrun, saturation and
                         AXI bus errors");
    commit e12ff7f3edf5 ("drm/malidp: Print message about insufficient
                         mclk as error");
    commit 0c3474eb7460 ("drm: mali-dp: Add reset support");
    commit d797dd764d5e ("drm: mali-dp: Add 40-bit DMA support");
    commit 25cc25d1a19b ("drm: mali-dp: Allow to connect several
                         encoders");
    commit 32df4665675b ("drm/mali-dp: Always setup yuv2rgb coefficients
                         for YUV formats").
    
    Issue: #MCOM03SW-2843
    Change-Id: Id8328a380a1db5e99801ca65e3df75c180c3f6e8
    Signed-off-by: Vladimir Terekhov <vterekhov@elvees.com>

--- drivers/gpu/drm/arm/malidp_drv.c
+++ drivers/gpu/drm/arm/malidp_drv.c
@@ -15,6 +15,8 @@
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
 #include <linux/debugfs.h>
+#include <linux/dma-mapping.h>
+#include <linux/reset.h>
 
 #include <drm/drm_atomic.h>
 #include <drm/drm_atomic_helper.h>
@@ -679,6 +681,8 @@ static int malidp_runtime_pm_suspend(struct device *dev)
 	clk_disable_unprepare(hwdev->aclk);
 	clk_disable_unprepare(hwdev->pclk);
 
+	reset_control_assert(hwdev->reset);
+
 	return 0;
 }
 
@@ -691,11 +695,34 @@ static int malidp_runtime_pm_resume(struct device *dev)
 	clk_prepare_enable(hwdev->pclk);
 	clk_prepare_enable(hwdev->aclk);
 	clk_prepare_enable(hwdev->mclk);
+
+	reset_control_deassert(hwdev->reset);
+
 	hwdev->pm_suspended = false;
 	malidp_de_irq_hw_init(hwdev);
 	malidp_se_irq_hw_init(hwdev);
 
 	return 0;
+}
+
+/* setup encoders possible_clones */
+static void malidp_cloning_configure(struct drm_device *drm,
+	struct malidp_drm *malidp)
+{
+	struct drm_encoder *encoder;
+	struct drm_encoder *mw_encoder = &malidp->mw_connector.encoder;
+
+	list_for_each_entry(encoder, &drm->mode_config.encoder_list, head) {
+		/* it will be warning if self mask is not set */
+		encoder->possible_clones |= drm_encoder_mask(encoder);
+
+		/* allow clonning with writeback connector for all encoders
+		   writeback encoder is virtual */
+		if (encoder->encoder_type != DRM_MODE_ENCODER_VIRTUAL) {
+			encoder->possible_clones |= drm_encoder_mask(mw_encoder);
+			mw_encoder->possible_clones |= drm_encoder_mask(encoder);
+		}
+	}
 }
 
 static int malidp_bind(struct device *dev)
@@ -706,11 +733,15 @@ static int malidp_bind(struct device *dev)
 	struct malidp_hw_device *hwdev;
 	struct platform_device *pdev = to_platform_device(dev);
 	struct of_device_id const *dev_id;
-	struct drm_encoder *encoder;
 	/* number of lines for the R, G and B output */
 	u8 output_width[MAX_OUTPUT_CHANNELS];
 	int ret = 0, i;
 	u32 version, out_depth = 0;
+
+	/* TODO: Check if all DP* IP's support 40-bit DMA addressing */
+	ret = dma_coerce_mask_and_coherent(dev, DMA_BIT_MASK(40));
+	if (ret)
+		return ret;
 
 	malidp = devm_drm_dev_alloc(dev, &malidp_driver, typeof(*malidp), base);
 	if (IS_ERR(malidp))
@@ -744,6 +775,10 @@ static int malidp_bind(struct device *dev)
 	hwdev->pxlclk = devm_clk_get(dev, "pxlclk");
 	if (IS_ERR(hwdev->pxlclk))
 		return PTR_ERR(hwdev->pxlclk);
+
+	hwdev->reset = devm_reset_control_get(dev, NULL);
+	if (IS_ERR(hwdev->reset))
+		return PTR_ERR(hwdev->reset);
 
 	/* Get the optional framebuffer memory resource */
 	ret = of_reserved_mem_device_init(dev);
@@ -824,14 +859,7 @@ static int malidp_bind(struct device *dev)
 		goto bind_fail;
 	}
 
-	/* We expect to have a maximum of two encoders one for the actual
-	 * display and a virtual one for the writeback connector
-	 */
-	WARN_ON(drm->mode_config.num_encoder > 2);
-	list_for_each_entry(encoder, &drm->mode_config.encoder_list, head) {
-		encoder->possible_clones =
-				(1 << drm->mode_config.num_encoder) -  1;
-	}
+	malidp_cloning_configure(drm, malidp);
 
 	ret = malidp_irq_init(pdev);
 	if (ret < 0)
@@ -918,20 +946,26 @@ static int malidp_compare_dev(struct device *dev, void *data)
 
 static int malidp_platform_probe(struct platform_device *pdev)
 {
-	struct device_node *port;
+	struct device_node *epoint;
 	struct component_match *match = NULL;
 
 	if (!pdev->dev.of_node)
 		return -ENODEV;
 
-	/* there is only one output port inside each device, find it */
-	port = of_graph_get_remote_node(pdev->dev.of_node, 0, 0);
-	if (!port)
-		return -ENODEV;
+	for_each_endpoint_of_node(pdev->dev.of_node, epoint) {
+		struct device_node *intf = of_graph_get_remote_port_parent(epoint);
 
-	drm_of_component_match_add(&pdev->dev, &match, malidp_compare_dev,
-				   port);
-	of_node_put(port);
+		if (!intf)
+			continue;
+
+		if (of_device_is_available(intf)) {
+			DRM_DEBUG_DRIVER("Adding component %s\n", intf->full_name);
+			drm_of_component_match_add(&pdev->dev, &match, malidp_compare_dev, intf);
+		}
+
+		of_node_put(intf);
+	}
+
 	return component_master_add_with_match(&pdev->dev, &malidp_master_ops,
 					       match);
 }
