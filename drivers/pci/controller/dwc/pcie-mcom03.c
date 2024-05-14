a6854cca04f25ccbcf519ebc2b2357e22a6894c7
Vladimir Terekhov <vterekhov@elvees.com>
2024-05-14 14:01:10 +0300

    PCI: dwc: mcom03: Add MCom-03 PCIe driver
    The driver added as of commit 63f38cd4885d
    ("arm64: dts: elvees: elvsmarccb: Use PWM backlight control").
    
    Issue: #MCOM03SW-2843
    Change-Id: If928d469f335b3b3efedead8a317514e674a287c
    Signed-off-by: Vladimir Terekhov <vterekhov@elvees.com>

---
+++ drivers/pci/controller/dwc/pcie-mcom03.c
@@ -0,0 +1,407 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * PCIe RC driver for MCom-03
+ *
+ * Copyright 2021-2023 RnD Center "ELVEES", JSC
+ */
+#include <linux/clk.h>
+#include <linux/delay.h>
+#include <linux/gpio.h>
+#include <linux/interrupt.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/mfd/syscon.h>
+#include <linux/module.h>
+#include <linux/of_device.h>
+#include <linux/of_gpio.h>
+#include <linux/pci.h>
+#include <linux/platform_device.h>
+#include <linux/resource.h>
+#include <linux/types.h>
+#include <linux/regmap.h>
+#include <linux/reset.h>
+
+#include "pcie-designware.h"
+
+#define SYS_CTRL_OFF			0x0
+#define SYS_CTRL_APP_LTSSM_EN		BIT(4)
+#define SYS_CTRL_OVRD_LTSSM_EN		BIT(31)
+#define SYS_CTRL_DEVICE_TYPE_MASK	GENMASK(3, 0)
+
+#define EDMA_INTx_INT			0x10
+#define EDMA_INTx_INT_ASSERTED		GENMASK(3, 0)
+#define EDMA_INTx_INT_DEASSERTED	GENMASK(7, 4)
+
+#define SYS_JESD_EN_OFF			0x300
+
+#define DEVICE_TYPE_EP	0
+#define DEVICE_TYPE_RC	4
+
+#define SDR_PCI0_CTL		0x50
+#define SDR_PCI1_CTL		0x54
+#define PCIE_BTN_RSTN		BIT(1)
+
+#define SDR_PCIE_PERSTN		0x150
+#define SDR_PCI0_PERSTN_MODE	BIT(0)
+#define SDR_PCI0_PERSTN		BIT(1)
+#define SDR_PCI1_PERSTN_MODE	BIT(8)
+#define SDR_PCI1_PERSTN		BIT(9)
+
+#define to_mcom03_pcie(x)	dev_get_drvdata((x)->dev)
+
+struct mcom03_pcie {
+	struct dw_pcie			*pci;
+	struct regmap			*sdr_base;
+	void __iomem			*apb_base;
+	u32				sdr_ctl_offset;
+	int				id;
+	struct reset_control		*reset;
+	struct irq_domain		*irq_domain;
+	struct gpio_desc		*reset_gpio;
+};
+
+static void mcom03_pcie_writel(struct mcom03_pcie *pcie, u32 reg, u32 val)
+{
+	writel(val, pcie->apb_base + reg);
+}
+
+static u32 mcom03_pcie_readl(struct mcom03_pcie *pcie, u32 reg)
+{
+	return readl(pcie->apb_base + reg);
+}
+
+static void mcom03_pcie_ltssm_toggle(struct mcom03_pcie *pcie, u32 val)
+{
+	u32 reg;
+
+	reg = mcom03_pcie_readl(pcie, SYS_CTRL_OFF);
+	reg &= ~SYS_CTRL_APP_LTSSM_EN;
+	reg |= SYS_CTRL_OVRD_LTSSM_EN | (val ? SYS_CTRL_APP_LTSSM_EN : 0);
+	mcom03_pcie_writel(pcie, SYS_CTRL_OFF, reg);
+}
+
+static int mcom03_pcie_host_init(struct pcie_port *pp)
+{
+	struct dw_pcie *pci = to_dw_pcie_from_pp(pp);
+	struct mcom03_pcie *pcie = to_mcom03_pcie(pci);
+
+	// Set BAR0/BAR1 to 4 KiB to preserve space in ranges
+	dw_pcie_writel_dbi2(pci, PCI_BASE_ADDRESS_0, 0xFFF);
+	dw_pcie_writel_dbi2(pci, PCI_BASE_ADDRESS_1, 0xFFF);
+	// Disable DBI_RO_WR_EN, since setup_rc() expects it to be off
+	dw_pcie_dbi_ro_wr_dis(pci);
+
+	dw_pcie_setup_rc(pp);
+	// If we need to program PHY, registers are programmed and
+	// app_hold_phy_rst is unset here
+	mcom03_pcie_ltssm_toggle(pcie, 1);
+	dw_pcie_wait_for_link(pci);
+
+	return 0;
+}
+
+static int mcom03_pcie_msi_host_init(struct pcie_port *pp)
+{
+	struct dw_pcie *pci = to_dw_pcie_from_pp(pp);
+	struct device *dev = pci->dev;
+	struct device_node *np = dev->of_node;
+	struct device_node *msi_node;
+
+	/*
+	 * The MSI domain is set by the generic of_msi_configure().  This
+	 * .msi_host_init() function keeps us from doing the default MSI
+	 * domain setup in dw_pcie_host_init() and also enforces the
+	 * requirement that "msi-parent" exists.
+	 */
+	msi_node = of_parse_phandle(np, "msi-parent", 0);
+	if (!msi_node) {
+		dev_err(dev, "failed to find msi-parent\n");
+		return -EINVAL;
+	}
+
+	of_node_put(msi_node);
+	return 0;
+}
+
+static void dw_plat_set_num_vectors(struct pcie_port *pp)
+{
+	pp->num_vectors = MAX_MSI_IRQS;
+}
+
+static const struct dw_pcie_host_ops mcom03_pcie_host_ops = {
+	.host_init = mcom03_pcie_host_init,
+	.msi_host_init = mcom03_pcie_msi_host_init,
+	.set_num_vectors = dw_plat_set_num_vectors,
+};
+
+static void mcom03_pcie_set_dev_type(struct mcom03_pcie *pcie,
+				     unsigned int device_type)
+{
+	u32 reg;
+
+	reg = mcom03_pcie_readl(pcie, SYS_CTRL_OFF);
+	reg &= ~SYS_CTRL_DEVICE_TYPE_MASK;
+	mcom03_pcie_writel(pcie, SYS_CTRL_OFF, reg | device_type);
+}
+
+static void mcom03_pcie_set_jesd_en_zero(struct mcom03_pcie *pcie)
+{
+	mcom03_pcie_writel(pcie, SYS_JESD_EN_OFF, 0);
+}
+
+static void mcom03_pcie_btn_reset(struct mcom03_pcie *pcie)
+{
+	regmap_update_bits(pcie->sdr_base, pcie->sdr_ctl_offset,
+			   PCIE_BTN_RSTN, 0);
+	regmap_update_bits(pcie->sdr_base, pcie->sdr_ctl_offset,
+			   PCIE_BTN_RSTN, PCIE_BTN_RSTN);
+}
+
+static void mcom03_pcie_unset_perst(struct mcom03_pcie *pcie)
+{
+	if (pcie->reset_gpio) {
+		/* "Power Sequencing and Reset Signal Timings" table in
+		 * PCI EXPRESS CARD ELECTROMECHANICAL SPECIFICATION, REV. 3.0
+		 * indicates PERST# should be deasserted after minimum of 100us
+		 * once REFCLK is stable. */
+		usleep_range(100, 200);
+		gpiod_set_value_cansleep(pcie->reset_gpio, 1);
+		usleep_range(1000, 1500);
+	}
+
+	if (pcie->id == 0)
+		regmap_update_bits(pcie->sdr_base, SDR_PCIE_PERSTN,
+				   SDR_PCI0_PERSTN_MODE | SDR_PCI0_PERSTN,
+				   SDR_PCI0_PERSTN);
+	else
+		regmap_update_bits(pcie->sdr_base, SDR_PCIE_PERSTN,
+				   SDR_PCI1_PERSTN_MODE | SDR_PCI1_PERSTN,
+				   SDR_PCI1_PERSTN);
+}
+
+static void mcom03_pcie_legacy_irq_handler(struct irq_desc *desc)
+{
+	struct mcom03_pcie *pcie = irq_desc_get_handler_data(desc);
+	struct irq_chip *chip = irq_desc_get_chip(desc);
+	unsigned long reg;
+	u32 virq, bit;
+
+	chained_irq_enter(chip, desc);
+	reg = mcom03_pcie_readl(pcie, EDMA_INTx_INT);
+
+	if (reg & EDMA_INTx_INT_ASSERTED) {
+		mcom03_pcie_writel(pcie, EDMA_INTx_INT, reg & EDMA_INTx_INT_ASSERTED);
+
+		for_each_set_bit(bit, &reg, PCI_NUM_INTX) {
+			virq = irq_find_mapping(pcie->irq_domain, bit);
+			if (virq)
+				generic_handle_irq(virq);
+		}
+	}
+
+	if (reg & EDMA_INTx_INT_DEASSERTED)
+		mcom03_pcie_writel(pcie, EDMA_INTx_INT, reg & EDMA_INTx_INT_DEASSERTED);
+
+	chained_irq_exit(chip, desc);
+}
+
+static int mcom03_pcie_intx_map(struct irq_domain *domain, unsigned int irq,
+				irq_hw_number_t hwirq)
+{
+	irq_set_chip_and_handler(irq, &dummy_irq_chip, handle_simple_irq);
+	irq_set_chip_data(irq, domain->host_data);
+	return 0;
+}
+
+static const struct irq_domain_ops intx_domain_ops = {
+	.map = mcom03_pcie_intx_map,
+	.xlate = pci_irqd_intx_xlate,
+};
+
+static int mcom03_pcie_config_legacy_irq(struct pcie_port *pp)
+{
+	struct dw_pcie *pci = to_dw_pcie_from_pp(pp);
+	struct device *dev = pci->dev;
+	struct mcom03_pcie *mcom03 = to_mcom03_pcie(pci);
+	struct device_node *node = dev->of_node;
+	struct device_node *intc;
+
+	intc = of_get_child_by_name(node, "legacy-interrupt-controller");
+	if (!intc) {
+		dev_err(dev, "No 'legacy-interrupt-controller' node found\n");
+		return -ENODEV;
+	}
+
+	mcom03->irq_domain = irq_domain_add_linear(intc, PCI_NUM_INTX,
+						   &intx_domain_ops, pp);
+	if (!mcom03->irq_domain) {
+		dev_err(dev, "Failed to create INTx IRQ domain\n");
+		return -EINVAL;
+	}
+
+	irq_set_chained_handler_and_data(pp->irq,
+					 mcom03_pcie_legacy_irq_handler, mcom03);
+
+	return 0;
+}
+
+static int mcom03_add_pcie_port(struct mcom03_pcie *pcie,
+				struct platform_device *pdev)
+{
+	struct dw_pcie *pci = pcie->pci;
+	struct pcie_port *pp = &pci->pp;
+	struct device *dev = &pdev->dev;
+	int ret;
+
+	// TODO: Add Hotplug, LEQ, and other IRQ support
+	pp->irq = platform_get_irq(pdev, 1);
+	if (pp->irq < 0) {
+		dev_info(dev, "Legacy IRQ not found: only MSI will work\n");
+	} else {
+		ret = mcom03_pcie_config_legacy_irq(pp);
+		if (ret < 0)
+			return ret;
+	}
+
+	pp->ops = &mcom03_pcie_host_ops;
+
+	mcom03_pcie_unset_perst(pcie);
+	mcom03_pcie_set_dev_type(pcie, DEVICE_TYPE_RC);
+	mcom03_pcie_set_jesd_en_zero(pcie);
+	mcom03_pcie_ltssm_toggle(pcie, 0);
+	// If we need to program PHY, app_hold_phy_rst is asserted here
+	mcom03_pcie_btn_reset(pcie);
+
+	ret = dw_pcie_host_init(pp);
+	if (ret) {
+		dev_err(dev, "Failed to initialize PCIe host\n");
+		return ret;
+	}
+
+	return 0;
+}
+
+static const struct dw_pcie_ops dw_pcie_ops = {
+};
+
+static int mcom03_pcie_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	struct mcom03_pcie *pcie;
+	struct dw_pcie *pci;
+	struct resource *res;
+	int ret;
+
+	pcie = devm_kzalloc(dev, sizeof(*pcie), GFP_KERNEL);
+	if (!pcie)
+		return -ENOMEM;
+
+	pci = devm_kzalloc(dev, sizeof(*pci), GFP_KERNEL);
+	if (!pci)
+		return -ENOMEM;
+
+	pci->dev = dev;
+	pci->ops = &dw_pcie_ops;
+
+	pcie->pci = pci;
+
+	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, "dbi");
+	pci->dbi_base = devm_ioremap_resource(dev, res);
+	pci->dbi_base2 = pci->dbi_base + 0x100000;
+	if (IS_ERR(pci->dbi_base)) {
+		dev_err(dev, "Failed to remap dbi memory\n");
+		return PTR_ERR(pci->dbi_base);
+	}
+
+	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, "apb");
+	pcie->apb_base = devm_ioremap_resource(dev, res);
+	if (IS_ERR(pcie->apb_base)) {
+		dev_err(dev, "Failed to remap apb memory\n");
+		return PTR_ERR(pcie->apb_base);
+	}
+
+	pcie->sdr_base = syscon_regmap_lookup_by_phandle(dev->of_node,
+						   "elvees,urb");
+	if (IS_ERR(pcie->sdr_base)) {
+		if (PTR_ERR(pcie->sdr_base) != -EPROBE_DEFER)
+			dev_err(dev, "Failed to initialize property\n");
+		return PTR_ERR(pcie->sdr_base);
+	}
+
+	ret = of_property_read_u32(dev->of_node, "elvees,ctrl-id", &pcie->id);
+	if (ret) {
+		dev_err(dev, "Not found elvees,ctrl-id property\n");
+		return ret;
+	}
+	switch (pcie->id) {
+	case 0:
+		pcie->sdr_ctl_offset = SDR_PCI0_CTL;
+		break;
+	case 1:
+		pcie->sdr_ctl_offset = SDR_PCI1_CTL;
+		break;
+	default:
+		dev_err(dev, "Invalid elvees,ctrl-id %u\n", pcie->id);
+		return -EINVAL;
+	}
+
+	pcie->reset_gpio = devm_gpiod_get_optional(dev, "reset", GPIOD_OUT_LOW);
+	if (IS_ERR(pcie->reset_gpio)) {
+		dev_err(dev, "Can't get 'reset-gpio'\n");
+		return PTR_ERR(pcie->reset_gpio);
+	} else if (!pcie->reset_gpio) {
+		dev_warn(dev, "'reset-gpio' isn't specified in PCIe node\n");
+		dev_info(dev, "PERST must be set with other means\n");
+	}
+
+	pcie->reset = devm_reset_control_array_get_shared(dev);
+	if (IS_ERR(pcie->reset)) {
+		if (PTR_ERR(pcie->reset) != -EPROBE_DEFER)
+			dev_err(dev, "Failed to get PCIe resets\n");
+		return PTR_ERR(pcie->reset);
+	}
+	ret = reset_control_deassert(pcie->reset);
+	if (ret) {
+		dev_err(dev, "Failed to deassert PCIe resets\n");
+		return ret;
+	}
+
+	platform_set_drvdata(pdev, pcie);
+
+	ret = mcom03_add_pcie_port(pcie, pdev);
+	if (ret)
+		reset_control_assert(pcie->reset);
+
+	return ret;
+}
+
+static int mcom03_pcie_remove(struct platform_device *pdev)
+{
+	struct mcom03_pcie *pcie = platform_get_drvdata(pdev);
+
+	mcom03_pcie_ltssm_toggle(pcie, 0);
+	reset_control_assert(pcie->reset);
+	return 0;
+}
+
+static const struct of_device_id mcom03_pcie_of_match[] = {
+	{ .compatible = "elvees,mcom03-pcie", },
+	{ /* sentinel */ },
+};
+
+MODULE_DEVICE_TABLE(of, mcom03_pcie_of_match);
+
+static struct platform_driver mcom03_pcie_driver = {
+	.driver = {
+		.name	= "mcom03-pcie",
+		.of_match_table = mcom03_pcie_of_match,
+		.suppress_bind_attrs = true,
+	},
+	.probe = mcom03_pcie_probe,
+	.remove = mcom03_pcie_remove,
+};
+module_platform_driver(mcom03_pcie_driver);
+
+MODULE_AUTHOR("RnD Center ELVEES, JSC <support@elvees.com>");
+MODULE_DESCRIPTION("MCom-03 PCIe host controller driver");
+MODULE_LICENSE("GPL v2");
