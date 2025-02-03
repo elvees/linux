0a1d25f5d37a96a926c551a9d62baf9cb41eaf7d
Ilya Popov <ipopov@elvees.com>
2025-02-03 15:48:06 +0300

    PCI: dwc: mcom03: Allow MSI controller selection
    Embedded MSI controller is used when 'msi-parent' and 'msi-map' DT
    properties are absent. Due to hardware bug rf#14059/MCOM03-1622(read
    MCom-03 Errata $6.2 "Loss of 4 LSB in Requester ID" for details) use of
    GIC ITS is limited so we add ability to select which controller to use
    for MSI interrupts: embedded MSI or GIC ITS.
    
    Based on commit 8d36e4375b38 ("PCI: dwc: mcom03: Allow MSI controller
    selection").
    
    Issue: #MCOM03SW-3356
    Signed-off-by: Ilya Popov <ipopov@elvees.com>
    Change-Id: Ia18627137be6eed4aff2c717cd9430821ba319c9

--- drivers/pci/controller/dwc/pcie-mcom03.c
+++ drivers/pci/controller/dwc/pcie-mcom03.c
@@ -101,30 +101,16 @@ static int mcom03_pcie_host_init(struct dw_pcie_rp *pp)
 
 static int mcom03_pcie_msi_host_init(struct dw_pcie_rp *pp)
 {
-	struct dw_pcie *pci = to_dw_pcie_from_pp(pp);
-	struct device *dev = pci->dev;
-	struct device_node *np = dev->of_node;
-	struct device_node *msi_node;
-
-	/*
-	 * The MSI domain is set by the generic of_msi_configure().  This
-	 * .msi_host_init() function keeps us from doing the default MSI
-	 * domain setup in dw_pcie_host_init() and also enforces the
-	 * requirement that "msi-parent" exists.
-	 */
-	msi_node = of_parse_phandle(np, "msi-parent", 0);
-	if (!msi_node) {
-		dev_err(dev, "failed to find msi-parent\n");
-		return -EINVAL;
-	}
-
-	of_node_put(msi_node);
 	return 0;
 }
 
 static const struct dw_pcie_host_ops mcom03_pcie_host_ops = {
 	.host_init = mcom03_pcie_host_init,
 	.msi_host_init = mcom03_pcie_msi_host_init,
+};
+
+static const struct dw_pcie_host_ops mcom03_pcie_host_ops_embed = {
+	.host_init = mcom03_pcie_host_init,
 };
 
 static void mcom03_pcie_set_dev_type(struct mcom03_pcie *pcie,
@@ -244,6 +230,7 @@ static int mcom03_add_dw_pcie_rp(struct mcom03_pcie *pcie,
 	struct dw_pcie *pci = pcie->pci;
 	struct dw_pcie_rp *pp = &pci->pp;
 	struct device *dev = &pdev->dev;
+	struct device_node *np = dev->of_node;
 	int ret;
 
 	// TODO: Add Hotplug, LEQ, and other IRQ support
@@ -256,7 +243,14 @@ static int mcom03_add_dw_pcie_rp(struct mcom03_pcie *pcie,
 			return ret;
 	}
 
-	pp->ops = &mcom03_pcie_host_ops;
+	if (of_property_read_bool(np, "msi-parent") ||
+	    of_property_read_bool(np, "msi-map")) {
+		pp->ops = &mcom03_pcie_host_ops;
+		dev_info(dev, "Using external MSI interrupt-controller\n");
+	} else {
+		pp->ops = &mcom03_pcie_host_ops_embed;
+		dev_info(dev, "Using embedded MSI interrupt-controller\n");
+	}
 
 	mcom03_pcie_unset_perst(pcie);
 	mcom03_pcie_set_dev_type(pcie, DEVICE_TYPE_RC);
