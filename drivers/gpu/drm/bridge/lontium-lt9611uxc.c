b0bab210400931280c6afde9a5830ce8ee35caae
Oleg Ivanov <oaivanov@elvees.com>
2025-11-12 14:41:23 +0300

    drm/bridge: lt9611uxc: Add workaround for EDID interrupt loss
    ELV-MC03-SMARC boards use an GPIO port expander with interrupt output
    with limited functionality (lack of latch register) - XL9555. It does
    not store information about interrupts that occur on the input GPIO
    ports.
    
    The interrupt from the LT9611UXC is connected to the input GPIO ports
    of XL9555 and in some cases may be lost during interrupt handling.
    
    This commit modifies detect callback of drm_bridge control functions,
    which checks if anything is attached to the bridge output. In this
    callback, information about the HDMI cable connection is read from
    a register that also contains EDID ready status. This commit get
    this status and update internal driver status.
    
    Issue: #MCOM03SW-3431
    Change-Id: I6c54fcc4dc52937df4d34aa30ca5a7971d0aa8e1
    Signed-off-by: Oleg Ivanov <oaivanov@elvees.com>

--- drivers/gpu/drm/bridge/lontium-lt9611uxc.c
+++ drivers/gpu/drm/bridge/lontium-lt9611uxc.c
@@ -450,6 +450,7 @@ static enum drm_connector_status lt9611uxc_bridge_detect(struct drm_bridge *brid
 	unsigned int reg_val = 0;
 	int ret;
 	bool connected = true;
+	bool edid = false;
 
 	lt9611uxc_lock(lt9611uxc);
 
@@ -458,8 +459,17 @@ static enum drm_connector_status lt9611uxc_bridge_detect(struct drm_bridge *brid
 
 		if (ret)
 			dev_err(lt9611uxc->dev, "failed to read hpd status: %d\n", ret);
-		else
+		else {
+			edid  = reg_val & BIT(0);
 			connected  = reg_val & BIT(1);
+		}
+	}
+	/* Due to the short interrupt signal duration, interrupt loss is
+	 * possible. Therefore, we'll add a EDID status check to this callback.
+	 */
+	if (!ret) {
+		lt9611uxc->edid_read = edid;
+		wake_up_all(&lt9611uxc->wq);
 	}
 	lt9611uxc->hdmi_connected = connected;
 
