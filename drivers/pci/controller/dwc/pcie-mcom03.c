c1fd7688ad7edd9e0d74240aa4d6ffaccef664ea
Ilya Popov <ipopov@elvees.com>
2026-04-10 12:02:27 +0300

    PCI: dwc: mcom03: Switch to irq_domain_create_linear()
    Follow treewide change in commit 4b5e1d97154d ("PCI: Switch to
    irq_domain_create_linear()").
    
    Issue: #MCOM03SW-4092
    Signed-off-by: Ilya Popov <ipopov@elvees.com>
    Change-Id: I65c3fcd992bad03a50de4090b1d315c67f7be7a7

--- drivers/pci/controller/dwc/pcie-mcom03.c
+++ drivers/pci/controller/dwc/pcie-mcom03.c
@@ -314,8 +314,9 @@ static int mcom03_pcie_config_legacy_irq(struct dw_pcie_rp *pp)
 		return -ENODEV;
 	}
 
-	mcom03->irq_domain = irq_domain_add_linear(intc, PCI_NUM_INTX,
-						   &intx_domain_ops, pp);
+	mcom03->irq_domain = irq_domain_create_linear(of_fwnode_handle(intc),
+						      PCI_NUM_INTX,
+						      &intx_domain_ops, pp);
 	of_node_put(intc);
 	if (!mcom03->irq_domain) {
 		dev_err(dev, "Failed to create INTx IRQ domain\n");
