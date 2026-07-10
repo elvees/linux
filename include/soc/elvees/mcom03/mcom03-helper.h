a9f4585af88406c12b010b432bd0713de403bf38
Dmitrii Salnikov <dasalnikov@elvees.com>
2026-07-10 12:09:41 +0300

    soc: elvees: Add mcom03r-otp driver
    The driver added as of commit 075b3f5e34e2 ("Merge "Merge tag
    'v6.6.143' into mcom03-6.6.y" into mcom03-6.6.y").
    
    Issue: #MCOM03SW-4287
    Change-Id: Ie5d422cd5b678875f306fbc55afca9bb0394d8bb
    Signed-off-by: Dmitrii Salnikov <dasalnikov@elvees.com>

---
+++ include/soc/elvees/mcom03/mcom03-helper.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright 2025 RnD Center "ELVEES", JSC
+ *
+ */
+
+#ifndef __SOC_MCOM03_HELPER_H
+#define __SOC_MCOM03_HELPER_H
+
+int mcom03_sprintf(char *buf, size_t *size, int *pos, const char *fmt, ...);
+
+#endif
